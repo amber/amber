f8f212eca80e24abda70574cd040bacb0c5ebe92
Nathan Dinsmore <queryselector@gmail.com>
2012-10-08 00:50:25 -0400

    added script.move transitions

--- app.js
+++ app.js
@@ -1212,8 +1212,12 @@ d.BlockStack = d.Class(d.Control, {
 		this.initElements('d-block-stack');
 	},
 	setPosition: function (x, y) {
+		this.element.style.WebkitTransition = 'top .3s ease, left .3s ease';
 		this.element.style.left = x + 'px';
 		this.element.style.top = y + 'px';
+		setTimeout(function () {
+			this.element.style.WebkitTransition = '';
+		}.bind(this), 300);
 		return this;
 	},
 	x: function () {
